432f788521a3c2d32b4b7a837f5f681d2acd489d
dbetebenner <dbetebenner@gmail.com>
2015-02-25 10:37:39 -0500

    Adding loading message to SGPdata

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+`.onLoad` <- 
+function(libname, pkgname) {
+}
+
+
+`.onAttach` <- 
+function(libname, pkgname) {
+	if (interactive()) {
+		packageStartupMessage('SGPdata ',paste(paste(unlist(strsplit(as.character(packageVersion("SGPdata")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),'  For help type: help("SGPdata")')
+	}
+}
