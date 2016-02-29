247736629543d48468cecf8d5120b7c9632d59a2
dbetebenner <dbetebenner@gmail.com>
2016-02-29 10:23:55 -0500

    Finalizing 13.0 for CRAN submission

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <- 
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('SGPdata ',paste(paste(unlist(strsplit(as.character(packageVersion("SGPdata")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (2-1-2016).  For help: >help("SGPdata")')
+		packageStartupMessage('SGPdata ',paste(paste(unlist(strsplit(as.character(packageVersion("SGPdata")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (3-1-2016).  For help: >help("SGPdata")')
 	}
 }
